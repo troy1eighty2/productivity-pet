f27782263eaeef3313bb37fabcd389fd7a4a5bf2
Troy Tran <troytran000@gmail.com>
2024-04-02 16:39:28 -0500

    sleep

--- friend.py
+++ friend.py
@@ -7,28 +7,40 @@ impath = 'C:\\Users\\troyt\\productivity-pet\\animations\\'
 window = tk.Tk()
 screen_width = window.winfo_screenwidth() - 200
 screen_height = window.winfo_screenheight() - 150
+resolution = [screen_width, screen_height]
 
-def animate(action, label, counter, screen_width, screen_height):
+def animate(action, event_type, label, counter, response):
     if counter < len(action):
         label.configure(image = action[counter])
         counter += 1
-        window.after(100,animate, action, label, counter,screen_width, screen_height)
+        if event_type == 1:
+            resolution[0] -= 10
+        elif event_type == 2:
+            resolution[0] += 10
+        window.geometry(f"100x100+{response[0]}+{response[1]}")
+        window.after(100,animate, action, event_type, label, counter, response)
     else:
         counter = 0
         return
 
 def event():
-    event_type = random.randint(0, 2)
+    event_type = random.randint(0, 3)
     if event_type == 0:
-        window.after(1000,animate, idle, label, counter, screen_width, screen_height)
-        window.geometry(f"100x100+{screen_width}+{screen_height}")
-    elif event_type == 1:
-        window.after(1000,animate, walk_negative, label, counter, screen_width, screen_height)
-        window.geometry(f"100x100+{screen_width}+{screen_height}")
-    elif event_type == 2:
-        window.after(1000,animate, walk_positive, label, counter, screen_width, screen_height)
-        window.geometry(f"100x100+{screen_width}+{screen_height}")
-    window.after(1000, event)
+        window.after(1000,animate, idle, event_type, label, counter, resolution)
+    elif event_type == 1 and resolution[0] > 150:
+        window.after(1000,animate, walk_negative, event_type, label, counter, resolution)
+    elif event_type == 2 and resolution[0] < window.winfo_screenwidth() - 199:
+        window.after(1000,animate, walk_positive, event_type, label, counter, resolution)
+        window.geometry(f"100x100+{resolution[0]}+{resolution[1]}")
+    elif event_type == 3:
+        window.after(100,animate, idle_to_sleep, event_type, label, counter, resolution)
+        window.after(1000,animate, sleep, event_type, label, counter, resolution)
+        window.after(7000,animate, sleep_to_idle, event_type, label, counter, resolution)
+
+    if event_type == 3:
+        window.after(10000, event)
+    else:
+        window.after(2000, event)
 
 
 def destroyWin():
